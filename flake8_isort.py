57cc2bce0f80bf9bbd042c6dbf0b4b1a2cf1d20a
Doug Beck <doug@douglasbeck.com>
2015-10-07 22:31:48 -0400

    Read from stdin

--- flake8_isort.py
+++ flake8_isort.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from isort import SortImports
+
+import sys
+
+
+class Read(object):
+
+    @classmethod
+    def file(cls, filename):
+        if filename == 'stdin':
+            return None
+        with open(filename, 'r') as f:
+            return f.read()
+
+    @classmethod
+    def tree(cls, tree):
+        return getattr(tree, 'source', None)
+
+    @classmethod
+    def stdin(cls):
+        content = sys.stdin.read()
+        if not content:
+            # issue: https://github.com/PyCQA/pep8/pull/443
+            print('Flake8Isort: stdin is empty')
+        return content
 
 
 class Flake8Isort(object):
@@ -7,10 +32,10 @@ class Flake8Isort(object):
     version = '0.1'
     message = 'I001 found unsorted imports'
 
-    def __init__(self, tree, filename):
-        self.filename = filename
+    def __init__(self, tree, filename, builtins=None):
+        self.content = Read.tree(tree) or Read.file(filename) or Read.stdin()
 
     def run(self):
-        sort_result = SortImports(self.filename, check=True)
+        sort_result = SortImports(file_contents=self.content, check=True)
         if sort_result.incorrectly_sorted:
             yield 0, 0, self.message, type(self)
